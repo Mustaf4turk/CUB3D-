ee8b2a963f11684d7d4c8811176d83648a81c3df
Mustafa Türk <muturk@k1m21s03.42istanbul.com.tr>
2026-03-09 19:11:28 +0300

    headers added XPDPDPD

--- include/parser_internal.h
+++ include/parser_internal.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_internal.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muturk <muturk@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 19:10:05 by muturk            #+#    #+#             */
+/*   Updated: 2026/03/09 19:10:28 by muturk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PARSER_INTERNAL_H
 # define PARSER_INTERNAL_H
 
@@ -25,33 +37,27 @@ typedef struct s_flood
 	int		w;
 }				t_flood;
 
-/* parser_utils.c */
 size_t	p_strlen(const char *s);
 int		p_is_space(char c);
 int		p_is_digit(char c);
 char	*p_dup_range(const char *s, int start, int end);
 void	p_free_lines(char **lines);
 
-/* parser_file.c */
 char	*p_read_all_file(const char *path);
 char	**p_split_lines(const char *all, int *line_count);
 int		p_is_blank_line(const char *line);
 
-/* parser_header.c */
 int		p_parse_header_line(t_game *game, const char *line);
 
-/* parser_elements.c */
 int		p_is_map_char(char c);
 int		p_is_map_line(const char *line);
 int		p_headers_complete(t_game *game);
 int		p_preparse_lines(t_game *game, t_parse *p);
 int		p_find_map_bounds(t_parse *p);
 
-/* parser_map.c */
 int		p_build_map_grid(t_game *game, t_parse *p);
 int		p_validate_char_and_spawn(t_game *game);
 
-/* parser_validate.c */
 int		p_validate_closed_map(t_game *game);
 int		p_check_tex_files(t_game *game);
 
